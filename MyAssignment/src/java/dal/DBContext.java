50359c6b4a058fa5a06251a2d35348da9d853862
tam5917 <taamn5917@gmail.com>
2025-03-03 23:39:15 +0700

    Update DBContext.java
    kết nối với cơ sở dữ liệu

--- MyAssignment/src/java/dal/DBContext.java
+++ MyAssignment/src/java/dal/DBContext.java
@@ -3,11 +3,34 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package dal;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-/**
- *
- * @author ASUS
- */
-public class DBContext {
+
+public abstract class DBContext<T> {
+    protected Connection connection;
+    public DBContext()
+    {
+        try {
+            String user = "sa";
+            String pass = "123";
+            String url = "jdbc:sqlserver://TÂM5917\\MSSQLSEVER01:1433;databaseName=assignmentdb;encrypt=true;trustServerCertificate=true;";
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            connection = DriverManager.getConnection(url, user, pass);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
     
+    public abstract ArrayList<T> list();
+    public abstract T get(int id);
+    public abstract void insert(T model);
+    public abstract void update(T model);
+    public abstract void delete(T model);
 }
+
+
