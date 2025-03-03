9610cfed30eab6e94268968d66e67dd6d298bdbe
tam5917 <taamn5917@gmail.com>
2025-03-03 23:45:29 +0700

    Create Employee.java
    lớp model đại diện cho nhân viên trong hệ thống, bao gồm ID, tên, email, phòng ban (Department), quản lý (manager) và danh sách nhân viên cấp dưới (staffs, directstaffs). Lớp này giúp quản lý mối quan hệ cấp trên - cấp dưới, hỗ trợ phân quyền và xét duyệt đơn nghỉ phép trong hệ thống.

---
+++ MyAssignment/src/java/model/Employee.java
@@ -0,0 +1,84 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author sonnt-local
+ */
+public class Employee {
+
+    private int id;
+    private String name;
+    private String email;
+    private Employee manager;
+    private Department dept;
+    private ArrayList<Employee> staffs = new ArrayList<>();
+    private ArrayList<Employee> directstaffs = new ArrayList<>();
+
+    public ArrayList<Employee> getDirectstaffs() {
+        return directstaffs;
+    }
+
+    public void setDirectstaffs(ArrayList<Employee> directstaffs) {
+        this.directstaffs = directstaffs;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Employee getManager() {
+        return manager;
+    }
+
+    public void setManager(Employee manager) {
+        this.manager = manager;
+    }
+
+    public Department getDept() {
+        return dept;
+    }
+
+    public void setDept(Department dept) {
+        this.dept = dept;
+    }
+
+    public ArrayList<Employee> getStaffs() {
+        return staffs;
+    }
+
+    public void setStaffs(ArrayList<Employee> staffs) {
+        this.staffs = staffs;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" + "id=" + id + ", name=" + name + ", email=" + email + ", manager=" + manager + ", dept=" + dept + ", staffs=" + staffs + ", directstaffs=" + directstaffs + '}';
+    }
+
+}
